a7363d820f5108645bb1dfaf92595196390ca7b7
jdc13 <jdchapman97@outlook.com>
2023-06-03 11:47:21 -0600

    UART coms on UBUNTU pi functioning

--- Uart_raspberry_pi.py
+++ Uart_raspberry_pi.py
@@ -4,6 +4,7 @@
 import serial
 import time
 
+print("josh can't tuype with lag")
 #with open('/boot/config.txt', 'a') as f:
  #   f.write('enable_uart=1\n')
   #  f.write('dtoverlay-uart3\n')
@@ -12,22 +13,22 @@ import time
     # is that I'm unsure what it does differently. I'm sure that what currently
     # is in place will always want pins 8 and 10
     # serial.Serial('/dev/ttyS0', 9600) 
-pico  = serial.Serial('/dev/serial0', 9600)
-esp32 = serial.Serial('/dev/serial1', 9600)
+# pico  = serial.Serial('/dev/serial0', 9600)
+pico = serial.Serial('/dev/serial0', 9600)
 
 run = True
 color = "green"
 while run == True:
     # Send colors over serialport to be interpretted
     if color == "green":
+        print("check")
         pico.write(b"green")
-        esp32.write(b"green")
+        print("did we make it?")
+        #esp32.write(b"green")
         print("GREEN written to both boards")
         time.sleep(1) 
-        pico_msg = pico.readline().decode().strip()
-        if pico_msg != "green received":
-            print("brian screwed up")
-            run = False
+        #pico_msg = pico.readline().decode().strip()
+
             
         print("success!")
         color = "yellow"
@@ -35,25 +36,19 @@ while run == True:
     
     if color == "yellow":
         pico.write(b"yellow")
-        esp32.write(b"yellow")
+        #esp32.write(b"yellow")
         print("YELLOW written to both boards")
         time.sleep(1)
-        if pico_msg != "yellow received":
-            print("brian screwed up")
-            run = False
 
         print("success!")
-        color = red
+        color = "red"
         time.sleep(1)
     
     if color == "red":
         pico.write(b"red")
-        esp32.write(b"red")
+        #esp32.write(b"red")
         print("RED written to both boards")
         time.sleep(1)
-        if pico_msg != "red received":
-            print("brian screwed up")
-            run = False
 
         print("success!")
         color = "green"
